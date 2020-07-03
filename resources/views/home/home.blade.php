8ce889ed0765694dcc0bbdbd72f4b279d4a2886d
gagumcerdas <gagahgumelar@gmail.com>
2020-07-04 02:31:59 +0700

    tambah dashboard

---
+++ resources/views/home/home.blade.php
@@ -0,0 +1,17 @@
+@extends('adminlte.master')
+
+@section('content')
+<div class="ml-3 mt-3 mr-3">
+    <div class="card card-primary">
+        <div class="card-header">
+          <h3 class="card-title">Selamat Datang!</h3>
+        </div>
+          <div class="card-body">
+            <div>
+                <h3>Admin larahub ^,^</h3>
+            </div>
+          </div>
+        </form>
+      </div>
+</div>
+@endsection
